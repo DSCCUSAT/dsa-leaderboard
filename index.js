a2abd2e800b047d4493ff4054dd18f8404d065d5
Mishal <mishal0404@gmail.com>
2023-09-04 14:05:26 +0530

    all done

--- index.js
+++ index.js
@@ -2,12 +2,70 @@ const express = require("express");
 const app = express();
 const puppeteer = require('puppeteer');
 var cors = require('cors');
-app.use(cors({origin: 'http://127.0.0.1:5500'}));
+app.use(cors({origin: 'http://127.0.0.1:5501'}));
+
+const { LeetCode } = require("leetcode-query");
+
+
+
+
+app.get('/check/:username', (req, res)=>{
+    const d = new Date();
+    let time1 = d.getTime();
+    const {username} = req.params
+    const leetcode = new LeetCode();
+    leetcode.user(username).then((user)=>{
+        if (user.recentSubmissionList == null){
+            const d1 = new Date();
+            let time2 = d1.getTime();
+            res.status(200).send({
+                username: username,
+                recents:"",
+                time: time2-time1
+            })
+        }
+        else {
+            /**
+            recents = Array.from(user.recentSubmissionList, el=>{
+                if (el.statusDisplay == "Accepted"){el.title.toLowerCase().replace(new RegExp(' ', 'g'),"-")}
+            })
+             */
+            recents = []
+            for (i in user.recentSubmissionList){
+                el = user.recentSubmissionList[i]
+                if (el.statusDisplay == "Accepted"){
+                   var k = el.title.toLowerCase().replace(new RegExp(' ', 'g'),"-")
+                   recents.push(k)
+                }
+            }
+            const d1 = new Date();
+            let time2 = d1.getTime();
+            recents = Array.from(new Set(recents))
+            res.status(200).send({
+                username: username,
+                recents:recents.toString(),
+                time: time2-time1
+            })
+        }
+        
+    })
+
+})
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 
+const joptions = {
+    "headers": {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+      "Accept-Encoding": "gzip, deflate, br", 
+      "Accept-Language": "en-US,en;q=0.9", 
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36", 
+    }
+  }
+
+  /**
 app.get('/check/:username', (req, res)=>{
 
     const {username} = req.params
@@ -19,6 +77,7 @@ app.get('/check/:username', (req, res)=>{
         const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
 
+        await page.setExtraHTTPHeaders(joptions.headers);
         await page.goto(url2);
         
         let recents = await page.evaluate(() => {
@@ -40,6 +99,7 @@ app.get('/check/:username', (req, res)=>{
         console.log(err);
     });
 })
+ */
 
 
 app.listen(
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import {initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js'
 import { getDatabase, set, get, ref, child, push, onValue } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js'
-
+  
+if (localStorage.getItem("admin") == "true"){
+    document.getElementById("update").style.display = "block"
+}
 
 const firebaseConfig = {
     apiKey: "AIzaSyC9iD6-1W67VoQORY1uc_Fssa8233loiig",
@@ -98,7 +101,7 @@ function saveuser(username, question) {
 */
 
 function saveuser(username, question){
-    
+
     set(ref(db, "users/"+username+"/"+question), {
         done: "true"
     });
@@ -115,12 +118,12 @@ var usernames = ["Mishal0404","user4713QR", "Nevin",
  "hridyaharshan", "Nandana07", "Khushi6517", "barash1311", "khdas567", "Anagas", 
  "singhmuskan16", "aadarsh_ps", "Nirmal050", "vishnu13431", "ansafraees", "mihalpalakkal", "aditripathi1357", "prince3654", "Naveen_002", "Dheerajdileep", "Jr_NewT ", "EvlynEbichan ", "AioonT", "Heavenly_Ben", "Ivinej", "2getsandesh", "rimasid", "Sadhnan", "karthiksujith789", "resikeshramachandran05", "Nandakishor-KV", "Najah_1_", "_treesa_george_", "anumini", "MeticulousVision", "mgnm", "aakashpr", "rahulpradeep001", "abhay-krishna", "dikshas2911", "sivadasrajan", "safwanmshereef7", "shubhamk45", "the_royal_Abhi9s", "Sonakshi2125", "adijithsabu", "asciiindex", "Archa_2000", "abiirram", "fidhaa_2003", "hrk2575", "nikhilakumar2003", "aswinpradeepc ", "shivangi_chaturvedi", "iamdeepakpm282", "ashwath_john", "nandana___santhosh", "abhigaprasad", "avinashsinghlilotra", "eldhoabraham", "Harikri12", "Ruvais_9296", "Thrishakannan", "nabeelnazeer", "Akshaykr008", "rabeeh_ta", "kalpanaroy1712", "omamaashraf1913111046", "harshedabdulla", "dakshinard3010", "Sharonsabu12", "Athi123", "Abhishek", "priyanshu__21", "Aiswarya ", "Aiswarya_4528", "Aazim_Anish", "ri5hi", "niteshmishra171171171", "Abhinandgit", "ghanashyamvn", "mad_techy", "anzal", "Anzal", "Sree_Laxmi-123", "Narthana", "Sidharth", "ReVuZ", "SayakPaul", "20cs076praj ", "adarshbytes", "arjunvaradiyil203", "007aswinak007", "alimanhal", "Rebin03", "Sreesayanth", 
  "Adithyasankar_2003", "satyamsrivastava6871", "bhavyagirish3", "Nikitha_Ragesh", "sharathj116", "Merin_Biju", "Gokul211468", "lucifer_sama", "sonascaria", "mehul20", "ashwinsudheer", "resikeshramachandran05", "NazneenT03", "athulya_anilkumar", "Adil9645", "Anjimakp0", "hafeefapc2003", "ShriyaBala", "aswathy_linz", "Siddeque", "pejayalakshmi2003", "sweetcaro", "abhijith8176", "nlkguy", "sthejas87", "rabeeh6", "Creedz", "jeri", "Sreeragpv_1729", "johnsonjoyal87", "aloysiuspattath", "parvathi_manari", "Ankan85", "Mamatha ", "shadaataj", "nandusn", "mrudulmathews", "Amal-Thilakan", "str_dolly", "Malavika_CS", "gayathrireji25", "gthm", "gangamangalassery1", "kumarmukesh14", "Anandkumar_21633", "Him_123-", "thepywizard", "Dhanushpk_50", "Jeslinpjames", "MdSaquibBakhshi", "Abhibav75478", "Krishnanunni_21", "potatomalik", "ithban", "gowrilekshmis", "Abna_Iqbal", "Ashkab_C", "dka001", "anasvemmully", "ankitdocode", "Shilpa_2000", "anusabu", "Anagha_kr_____", "sindhupr2003", "amalragh", "Himna_01_K", "Gopika12", "aastha_das15", "steve_godly", "arnav27122002", "mariya-123", "Radhakrishna ", "Nandana_Pradeep", "AkshayRaj1107", "nazal", "AkshayRaj1107", "sweetchaos_sin", "Swathy_shabu", "dharsan", "gaurav_4107", "Arjun_chilambath", "Tejaas_21", "aditya773", "Tejaas_21", "adhidev02", "Uttam_001", "tanu2110", "danwand47", "Aadithya_krishnan", "sameerabdulla175", "shantanu_01", "bhagyajkumar1", "Thennal", "neha1610", "g3kul", "favas_tp", "thasnimp", "Aswinlalks", "shadinkvr2002", "sreedevmannarkkad", "binilvb1234", "fimil", "leahfrancis", "rizal28", "thamannasiraj27", 
- "Sanjay ", "albinantonygeorge", "aleenadavid", "Athira", "fisa01", "Mamatha", "sivagangagj", "sreenandump", "Abhi_Chekavar", "MuhammedYahiya", "sanjayns7781", "NavyaBiju", "the_royal_abhi9s", "Anantheshbhat", "Midhun2001", "GangaV23", "r_i_a_s_r_i_a", "ajayalathiyur24 ", "devikamohan", "Anjimakp", "kirannarayanks", "namitapj", "Aneenajoshy", "salva_pk", "damianantony", "mhdnasim", "NavyaBinu", "harikrishnan1998", "Night_Glow", "athulchandran", "Sneha_Eliza", "AbhiramRam", "SeniorGatto", "Gayathri__k_", "SeniorGatto", "mohitpradeep2003", "chinmay2k3v", "m_rizwan", "SachuSam23", "Adithya_2508", "gowrinair78", "GauthamGK", "user8764qC", "adharshreghu2002", "niteshmishra171171171", "adharshreghu2002", "aarjunb123", "Soorya", "Jumana_Sherin", "AksAA2810", "shazzzzz", "va1shnav", "adithyanappu1515", "Arjun", "Vismaya.a.m.", "AleenaPascal", "Nazva", "rahulraj24", "dhyan08", "ShreyaN15", "rahulraj24", "rohitkumarrk1600", "ajmal_a_man", "Ancy12", "Harisankar_s", "Harisankar_s", "sephinse", "souloftheserpent", "sebastianmelbin67", "nandanasbhaskar", "29-ark", "_arjun_ajayakumar_", "lateralraise", "dreamerremaerdjo", "Susan_Jeevan_Philip", "abinfranci5", "muneebv", "user6614kC", "zaman_27", "Athira874", "Harshranjansinha", "steff001", "muhammedrisin", "cenosh", "ashiashish", "vaish02", "CodeBreak-Matrix", "mohammedajmal996", "briz_02", "M4NIAC", "lazygupta", "Mahith_Raveendran", "athiraathu14052001", "salihedneer", "Yash_Bhadauria", "athiraathu14052001", "Yash_Bhadauria", "amrutha8654", "sreenathkp", "VarshaShaheen", "MuhammedShaahid", "ronythomas2002", "haafiiz", "abraham98", 
+ "Sanjay ", "albinantonygeorge", "aleenadavid", "Athira", "fisa01", "Mamatha", "sivagangagj", "sreenandump", "Abhi_Chekavar", "MuhammedYahiya", "sanjayns7781", "NavyaBiju", "the_royal_abhi9s", "Anantheshbhat", "Midhun2001", "GangaV23", "r_i_a_s_r_i_a", "ajayalathiyur24 ", "devikamohan", "Anjimakp", "kirannarayanks", "namitapj", "Aneenajoshy", "salva_pk", "damianantony", "mhdnasim", "NavyaBinu", "harikrishnan1998", "Night_Glow", "athulchandran", "Sneha_Eliza", "AbhiramRam", "SeniorGatto", "Gayathri__k_", "SeniorGatto", "mohitpradeep2003", "chinmay2k3v", "m_rizwan", "SachuSam23", "Adithya_2508", "gowrinair78", "GauthamGK", "user8764qC", "adharshreghu2002", "niteshmishra171171171", "adharshreghu2002", "aarjunb123", "Soorya", "Jumana_Sherin", "AksAA2810", "shazzzzz", "va1shnav", "adithyanappu1515", "Arjun", "Vismaya.a.m.", "AleenaPascal", "Nazva", "rahulraj24", "dhyan08", "ShreyaN15", "rahulraj24", "rohitkumarrk1600", "ajmal_a_man", "Ancy12", "Harisankar_s", "sephinse", "souloftheserpent", "sebastianmelbin67", "nandanasbhaskar", "29-ark", "_arjun_ajayakumar_", "lateralraise", "dreamerremaerdjo", "Susan_Jeevan_Philip", "abinfranci5", "muneebv", "user6614kC", "zaman_27", "Athira874", "Harshranjansinha", "steff001", "muhammedrisin", "cenosh", "ashiashish", "vaish02", "CodeBreak-Matrix", "mohammedajmal996", "briz_02", "M4NIAC", "lazygupta", "Mahith_Raveendran", "athiraathu14052001", "salihedneer", "Yash_Bhadauria", "athiraathu14052001", "Yash_Bhadauria", "amrutha8654", "sreenathkp", "VarshaShaheen", "MuhammedShaahid", "ronythomas2002", "haafiiz", "abraham98", 
  "Gowrykrishna ", "pranav", "sabin_2002", "Adith628", "Gaiuzz", "Nandana_Pradeep", "briz_02", "anjanasuresh091", "aswinsreekanth11", "___sajidha", "BLANK", "AnmolRatna","AmeeshaArackal","effinjoewilson","abijithtk1212","salva_pk","sreedevmannarkkad"]
 
 
 
-var questions = ["find-the-duplicate-number", "running-sum-of-1d-array","sort-colors","move-zeroes","reverse-string","valid-palindrome-ii","valid-palindrome"]
+var questions = ["find-the-duplicate-number", "running-sum-of-1d-array"/**,"sort-colors","move-zeroes","reverse-string","valid-palindrome-ii","valid-palindrome" */]
 
 
 function arrayRemove(arr, value) {
@@ -143,7 +146,7 @@ get(child(dbRef, `users`)).then((snapshot) => {
         var points = Object.keys(snapshot.val()[i]).length
     }
 
-    var keysSorted = Object.keys(format).sort(function(a,b){return format[a]-format[b]})
+    var keysSorted = Object.keys(format).sort(function(a,b){return format[b]-format[a]})
     for (let i in keysSorted){
         var newcolors = arrayRemove(colors, randomcolor)
         randomcolor = newcolors[(Math.floor(Math.random() * colors.length))]
@@ -163,7 +166,7 @@ get(child(dbRef, `users`)).then((snapshot) => {
   }
 })
 
-var todaysquestion = "running-sum-of-1d-array"
+var todaysquestion = "find-the-duplicate-number"
 
 function update(){
     var total = usernames.length
@@ -176,9 +179,11 @@ function update(){
             //for (var j in questions){
                 var recents = result.recents.split(",")
                 var user = result.username
-                if (recents.includes(todaysquestion)){
-                    saveuser(user, todaysquestion)
-                    console.log("saved")
+                for (var j in questions){
+                    if (recents.includes(questions[j])){
+                        saveuser(user, questions[j])
+                        console.log("saved")
+                    }
                 }
             //}
             //console.log(recents)
